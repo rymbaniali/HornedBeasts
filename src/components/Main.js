16fc72791470e36ebd48a5a38d0570c403682e70
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-06 18:56:38 +0300

    added photo to tha app

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
-class Main extends React.Component{
-    render(){
-        return(
-            <main>
-               <h2> UniWhal </h2>
-               <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="img1" />
+class Main extends React.Component {
+    render() {
 
-            <p>
-            A unicorn and a narwhal nuzzling their horns",
-    "keyword": "narwhal
-            </p>
+        return (
+            <main>
+                <h2> UniWhal </h2>
+                <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="img1" />
+
+                <p> A unicorn and a narwhal nuzzling their horns" "keyword": "narwhal </p>
+
+                <h2>rhino</h2>
+                <img src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" /> ;
+                <p> "Mother (or father) rhino with two babies" </p>
+
+                <h2>Unicorn Head</h2>
+                <img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" /> ;
+                <p> "Someone wearing a creepy unicorn head mask" </p>
+
             </main>
         )
     }
