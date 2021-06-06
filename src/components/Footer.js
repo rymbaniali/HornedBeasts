ba2a3f716051bc72620cd6374c29ddfd7206be5a
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-06 17:34:46 +0300

    creat app

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Footer extends React.Component{
+    render() {
+        return(
+            <footer>
+                <span> By REEM BANI ALI </span>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <main>
+               <h2> UniWhal </h2>
+               <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="img1" />
+
+            <p>
+            A unicorn and a narwhal nuzzling their horns",
+    "keyword": "narwhal
+            </p>
+            </main>
+        )
+    }
+}
+export default Main;
